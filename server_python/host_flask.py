626cfa9859e2e326b2b57e2c915ebab2a66f7163
wolvever <wolvever@gmail.com>
2024-08-13 14:53:57 +0800

    Dynamically load flask

--- server_python/host_flask.py
+++ server_python/host_flask.py
@@ -1,25 +1,24 @@
 from typing import Dict
-from flask import Flask
-
 from server_python.app import App, AppSetting
 from server_python.host import Host
 
 
 class FlaskHost(Host):
+    from flask import Flask
 
-    def __init__(self, flask = None, flask_config: Dict = {} ):
-        if flask is None:
-            flask = Flask(__name__, **flask_config)
+    def __init__(self, flask_obj = None, flask_config: Dict = {}):
+        if flask_obj is None:
+            flask_obj = self.Flask(__name__, **flask_config)
         
-        self.flask = flask
+        self.app = flask_obj
         self.apps = {}
 
     def config(self, setting: AppSetting):
         self.setting = setting
-        self.flask.config.from_object(setting)
+        self.app.config.from_object(setting)
         
     def add_app(self, app: App):
-        app.register(self.flask)
+        app.register(self.app)
         app_name = app.__class__.__name__
         self.apps[app_name] = app
     
@@ -27,11 +26,11 @@ class FlaskHost(Host):
         return self.apps.get(app_name)
     
     def get_runtime(self):
-        return self.flask
+        return self.app
 
     def run(self):
         setting = self.setting.server
-        self.flask.run(
+        self.app.run(
             host=setting.host, 
             port=setting.port, 
             debug=setting.debug,
